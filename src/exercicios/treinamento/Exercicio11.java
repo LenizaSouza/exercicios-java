728e90443b28e2eb319d9006c2c662004981b07f
Leniza <lenizarbsz@gmail.com>
2022-11-16 21:11:11 -0300

    Exercicio11

--- src/exercicios/treinamento/Exercicio11.java
+++ src/exercicios/treinamento/Exercicio11.java
@@ -1,4 +1,7 @@
 package exercicios.treinamento;
+
+import java.util.Scanner;
+
 /* Exercício 11
  * As organizações CSM resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes.  
 a. Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual;
@@ -17,7 +20,33 @@ d. O novo salário, após o aumento.
 public class Exercicio11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner leitor = new Scanner(System.in);
+
+		float salario, resultado;
+
+		System.out.println("Digite o salario do colaborador: ");
+		salario = leitor.nextFloat();
+
+		if (salario <= 280) {
+			resultado = (salario * 20) / 100;
+			System.out
+					.println("o salario é R$ " + salario + ", o percentual de aumento é de 20% e o aumento será de R$ "
+							+ resultado + " o salario com o reajuste será de R$ " + (salario + resultado));
+		} else if (salario <= 700) {
+			resultado = (salario * 15) / 100;
+			System.out
+					.println("o salario é R$ " + salario + ", o percentual de aumento é de 15% e o aumento será de R$ "
+							+ resultado + " o salario com o reajuste será de R$ " + (salario + resultado));
+		} else if (salario <= 1500) {
+			resultado = (salario * 10) / 100;
+			System.out
+					.println("o salario R$ é " + salario + ", o percentual de aumento é de 10% e o aumento será de R$ "
+							+ resultado + " o salario com o reajuste será de R$ " + (salario + resultado));
+		} else {
+			resultado = (salario * 5) / 100;
+			System.out.println("o salario R$ é " + salario + ", o percentual de aumento é de 5% e o aumento será de R$ "
+					+ resultado + " o salario com o reajuste será de R$ " + (salario + resultado));
+		} 
 
 	}
 
